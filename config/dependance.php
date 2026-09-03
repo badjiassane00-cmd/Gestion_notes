773eda0eeee6815073b580425ff98db418bb17b7
Ton Prenom Nom <ton@email.com>
2026-09-03 06:23:59 +0000

    refactor: extraire le dispatch dans Router et alleger index.php a l'assemblage des dependances

--- config/dependance.php
+++ config/dependance.php
@@ -1,8 +1,24 @@
 <?php
 
-
 $chemin = dirname(__DIR__);
 
 require_once $chemin . '/vendor/autoload.php';
 
 \Dotenv\Dotenv::createImmutable($chemin)->safeLoad();
+
+use App\Controller\CopieExamenController;
+use App\Repository\Database;
+use App\Repository\PdoCopieExamenRepository;
+use App\Router\Router;
+use App\Service\CalculNoteAvecRetardService;
+use App\Service\SoumissionCopieService;
+
+$configuration = require $chemin . '/config/database.php';
+
+$pdo = Database::getInstance($configuration)->getConnection();
+
+$repository = new PdoCopieExamenRepository($pdo);
+$service = new SoumissionCopieService(new CalculNoteAvecRetardService(), $repository);
+$controller = new CopieExamenController($service, $repository);
+
+(new Router())->dispatcher($controller);
