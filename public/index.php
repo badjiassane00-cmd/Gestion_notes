dcc5f4283faf3d01a99ebb69e221d2a4cef91e83
Ton Prenom Nom <ton@email.com>
2026-09-03 06:46:32 +0000

    fix: remplacer l'ancien routage manuel par l'appel au bootstrap

--- public/index.php
+++ public/index.php
@@ -1,31 +1,3 @@
 <?php
 
 require_once dirname(__DIR__) . '/config/dependance.php';
-
-use App\Controller\CopieExamenController;
-use App\Repository\Database;
-use App\Repository\PdoCopieExamenRepository;
-use App\Service\CalculNoteAvecRetardService;
-use App\Service\SoumissionCopieService;
-
-$pdo = Database::getInstance([])->getConnection();
-
-$repository = new PdoCopieExamenRepository($pdo);
-$service = new SoumissionCopieService(new CalculNoteAvecRetardService(), $repository);
-$controller = new CopieExamenController($service, $repository);
-
-$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-$methode = $_SERVER['REQUEST_METHOD'];
-
-if ($uri === '/copies' && $methode === 'GET') {
-    $controller->afficherListe();
-} elseif ($uri === '/copies' && $methode === 'POST') {
-    $controller->soumettre($_POST);
-} elseif (preg_match('#^/copies/(\d+)$#', $uri, $matches) && $methode === 'GET') {
-    $controller->afficherDetail((int) $matches[1]);
-} elseif ($uri === '/' || $uri === '/copies/nouvelle') {
-    $controller->afficherFormulaire();
-} else {
-    http_response_code(404);
-    require __DIR__ . '/../templates/erreur.php';
-}
